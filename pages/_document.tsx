b7ad624f0eff7abd3f2a75afe095523ea8efb6a0
Abhishek Saha <abhisheksaha11@gmail.com>
2020-10-15 14:18:40 +0200

    remove fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -42,14 +42,14 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           {/* PWA primary color */}
 
-          <link
+          {/* <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,500&display=swap"
           />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Lora|Roboto:300,400,500,700&display=swap"
-          />
+          /> */}
         </Head>
         <body>
           <Main />
