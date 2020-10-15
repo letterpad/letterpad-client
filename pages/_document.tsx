647968eb71bf6cca09ef418e35f64dba80ce04f2
Abhishek Saha <abhisheksaha11@gmail.com>
2020-10-15 11:55:48 +0200

    add inline styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,40 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const styledComponentsSheet = new ServerStyleSheet();
+
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            styledComponentsSheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+
+            {styledComponentsSheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      styledComponentsSheet.seal();
+    }
+  }
   render() {
     return (
       <Html lang="en" dir="ltr">
@@ -23,32 +56,5 @@ export default class MyDocument extends Document {
         </body>
       </Html>
     );
-  }
-}
-
-export async function getStaticProps(ctx) {
-  const styledComponentsSheet = new ServerStyleSheet();
-
-  const originalRenderPage = ctx.renderPage;
-
-  try {
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) =>
-          styledComponentsSheet.collectStyles(<App {...props} />),
-      });
-    const initialProps = await Document.getInitialProps(ctx);
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-
-          {styledComponentsSheet.getStyleElement()}
-        </>
-      ),
-    };
-  } finally {
-    styledComponentsSheet.seal();
   }
 }
