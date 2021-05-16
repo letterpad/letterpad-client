b39ca61b4a340bd5bfb09655ea358737ef90d9ad
Abhishek Saha <abhisheksaha11@gmail.com>
2021-05-16 17:29:07 +0530

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
-    API_URL: "http://localhost:3000/admin/api/graphql",
-    // API_URL: "https://letterpad.app/admin/api/graphql",
+    // API_URL: "http://localhost:3000/admin/api/graphql",
+    API_URL: "https://letterpad.app/admin/api/graphql",
     CLIENT_ID:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjE5OTQ4ODE4fQ.9o5uPKKyuT-56byjI-IIjEoELK2QKG6xMF9_tLvYxd8",
   },
