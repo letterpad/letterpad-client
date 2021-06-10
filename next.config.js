07aef871335e7c0cd55e90b842a0bc9ee272700c
Abhishek Saha <abhisheksaha11@gmail.com>
2021-06-10 19:58:44 +0530

    save api url

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
-    API_URL: "http://localhost:3000/admin/api/graphql",
-    // API_URL: "https://letterpad.app/admin/api/graphql",
+    // API_URL: "http://localhost:3000/admin/api/graphql",
+    API_URL: "https://letterpad.app/admin/api/graphql",
     CLIENT_ID:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjIyOTY1MDA3fQ.ayxsdhK5azkRyRcPXZj0ZvkbSqoqL6UvbJWP7QUgp8A",
   },
